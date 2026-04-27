63ba5dd54fa9b6ef151619091f66c8e05c2cbb9f
Andrei Briceag <andrei98briceag@gmail.com>
2026-04-27 13:17:28 +0200

    SetUp

---
+++ src/test/java/andrei/epic_energy_services/EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package andrei.epic_energy_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
