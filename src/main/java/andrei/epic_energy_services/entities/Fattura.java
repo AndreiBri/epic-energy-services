be2dfdf350192f812ce41b1091192ec8a19a2ba4
Christopher F A <christopher.armani095@gmail.com>
2026-04-28 16:38:14 +0200

    fattura entity

--- src/main/java/andrei/epic_energy_services/entities/Fattura.java
+++ src/main/java/andrei/epic_energy_services/entities/Fattura.java
@@ -1,4 +1,46 @@
 package andrei.epic_energy_services.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "fatture",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "unique_numero_fattura_per_cliente",
+                        columnNames = {"id_cliente", "numero"}
+                )
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Fattura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_fattura")
+    private UUID idFattura;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @Column(name = "data_creazione", nullable = false)
+    private LocalDate dataCreazione;
+
+    @Column(nullable = false)
+    private Long importo;
+
+    @Column(nullable = false)
+    private Integer numero;
+
+    @Column(name = "stato_custom", nullable = false)
+    private String statoCustom;
 }
