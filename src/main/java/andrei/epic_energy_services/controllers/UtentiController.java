8eb97afe99b82513201e0ec742119fa1252b7d08
Niccolò <niccolo.sorrenti@gmail.com>
2026-04-29 14:23:57 +0200

    commit

--- src/main/java/andrei/epic_energy_services/controllers/UtentiController.java
+++ src/main/java/andrei/epic_energy_services/controllers/UtentiController.java
@@ -3,10 +3,12 @@ package andrei.epic_energy_services.controllers;
 import andrei.epic_energy_services.payloads.in_response.UtenteResponseDTO;
 import andrei.epic_energy_services.services.UtentiService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/utenti")
@@ -23,6 +25,11 @@ public class UtentiController {
         return utentiService.findAll().stream().map(utente -> {
             return new UtenteResponseDTO(utente);
         }).toList();
+    }
+
+    @GetMapping("/{id}")
+    public UtenteResponseDTO findById(@PathVariable UUID id) {
+        return utentiService.findById(id);
     }
 
     
