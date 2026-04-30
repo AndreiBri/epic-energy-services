3cba6ef41d74a98a28a6e77da46985061b966cb3
Andrei Briceag <andrei98briceag@gmail.com>
2026-04-30 14:55:24 +0200

    Added Proper Error hendler

--- src/main/java/andrei/epic_energy_services/services/RuoliCustomService.java
+++ src/main/java/andrei/epic_energy_services/services/RuoliCustomService.java
@@ -3,6 +3,7 @@ package andrei.epic_energy_services.services;
 import andrei.epic_energy_services.entities.RuoloCustom;
 import andrei.epic_energy_services.entities.Utente;
 import andrei.epic_energy_services.entities.UtenteRuoloCustom;
+import andrei.epic_energy_services.exceptions.InvalidDataException;
 import andrei.epic_energy_services.exceptions.NotFoundException;
 import andrei.epic_energy_services.repositories.RuoliCustomRepository;
 import andrei.epic_energy_services.repositories.UtenteRuoloCustomRepository;
@@ -14,15 +15,12 @@ import java.util.UUID;
 
 @Service
 public class RuoliCustomService {
-    
-    @Autowired
-    private UtentiService utentiService;
-    
-    
-    @Autowired
-    private UtenteRuoloCustomRepository utenteRuoloCustomRepository;
 
     private final RuoliCustomRepository ruoloCustomRepository;
+    @Autowired
+    private UtentiService utentiService;
+    @Autowired
+    private UtenteRuoloCustomRepository utenteRuoloCustomRepository;
 
     public RuoliCustomService(RuoliCustomRepository ruoloCustomRepository) {
         this.ruoloCustomRepository = ruoloCustomRepository;
@@ -43,33 +41,31 @@ public class RuoliCustomService {
     public RuoloCustom create(RuoloCustom ruolo) {
 
         if (ruoloCustomRepository.existsByRuoloCustom(ruolo.getRuoloCustom())) {
-            throw new RuntimeException("Ruolo già esistente");
+            throw new InvalidDataException("Ruolo già esistente");
         }
 
         return ruoloCustomRepository.save(ruolo);
     }
-    
-    
+
 
     /**
      * Aggiungi un'associazione tra ruolo custom e utente.
      */
-    public void aggiungiAssociazioneTraRuoloUtenteEUtente(UUID ruoloCustomId, UUID utenteId) throws NotFoundException
-    {
+    public void aggiungiAssociazioneTraRuoloUtenteEUtente(UUID ruoloCustomId, UUID utenteId) throws NotFoundException {
         // cosa significa aggiungere un'associazione tra ruolo utente e utente?
         // abbiamo fatto un'entità che associa ruolo utente e utente
         // aggiungere questo oggetto in DB
         // come lo creo l'oggetto?    
-        
+
         // prima cerco l'utente per vedere se esiste
-        
+
         Utente utente = this.utentiService.findById(utenteId);
         RuoloCustom ruoloCustom = this.findById(ruoloCustomId);
-        
+
         UtenteRuoloCustom utenteRuoloCustom = new UtenteRuoloCustom(utente, ruoloCustom);
-        
+
         this.utenteRuoloCustomRepository.save(utenteRuoloCustom);
     }
-    
-    
+
+
 }
